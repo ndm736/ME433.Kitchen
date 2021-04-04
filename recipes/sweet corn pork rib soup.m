b7ef1ad77e5ccb98a437275f9e00f10b2244fd6a
Pijuan Yu <81704186+pijuanyu2022@users.noreply.github.com>
2021-04-04 00:37:20 -0500

    Update and rename sweet corn pork soup.m to sweet corn pork rib soup.m

--- recipes/sweet corn pork rib soup.m
+++ recipes/sweet corn pork rib soup.m
@@ -6,19 +6,16 @@ Text can be **bold**, _italic_, or ~~strikethrough~~.
 
 [Link to another page](./another-page.html).
 
-There should be whitespace between paragraphs.
+# Pijuan's Sweet Corn Pork Rib Soup
 
-There should be whitespace between paragraphs. We recommend including a README, or a file with information about your project.
+This dish is a traditional Chinese cuisine. The whole process of cooking this dish is really simple. 
+This soup is delicious and beneficial for your health.
+## Ingredients
 
-# Header 1
-
-This is a normal paragraph following a header. GitHub is a code hosting platform for version control and collaboration. It lets you and others work together on projects from anywhere.
-
-## Header 2
-
-> This is a blockquote following a header.
->
-> When something is important enough, you do it even if the odds are not in your favor.
+> Corn
+> Pork rib
+> Salt
+> Ginger
 
 ### Header 3
 
